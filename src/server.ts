919dfb4793f199eeece05660bbc302898f426914
Parvez Kose <parvez.kosex@gmail.com>
2026-01-31 16:49:54 -0500

    fix helmet type issue

--- src/server.ts
+++ src/server.ts
@@ -5,23 +5,29 @@ import habitRoutes from './routes/habitRoutes.ts'
 import cors from 'cors'
 import morgan from 'morgan'
 import helmet from 'helmet'
+import type { RequestHandler } from 'express'
 import { isTest } from '../env.ts'
 import { authenticateToken } from './middleware/auth.ts'
 import { APIError, errorHandler } from './middleware/errorHandler.ts'
 
 const app = express()
-app.use(helmet())
+//app.use(helmet())
+app.use((helmet as (options?: object) => RequestHandler)())
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(
   morgan('dev', {
     skip: () => isTest(),
-  })
+  }),
 )
 
+/* app.use((_, __, next) => {
+  next(new APIError('validation error', 'ValidationError', 400))
+}) */
+
 app.get('/health', (req, res) => {
-  res.send('<button>another</button>')
+  res.send('ok')
 })
 
 app.use('/api/auth', authRoutes)
